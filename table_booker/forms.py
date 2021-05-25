9888713f9264b51b7550d03e0a67036855a2628a
Sarkash Talib <Sarkash.talib@gmail.com>
2021-05-25 11:24:27 +0100

    Save booking ability

--- table_booker/forms.py
+++ table_booker/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db.models.base import Model
+
+from .models import Booking
 
 
 class UserForm(UserCreationForm):
@@ -25,3 +28,9 @@ class UserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ("table", "date")
