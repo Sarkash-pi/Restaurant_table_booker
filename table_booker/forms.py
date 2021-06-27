98663a974275b71c560df2c58944ed1fc113adb3
Sarkash Talib <Sarkash.talib@gmail.com>
2021-06-27 21:59:54 +0100

    prevent booking in the past and less than minimum guests

--- table_booker/forms.py
+++ table_booker/forms.py
@@ -46,6 +46,8 @@ class BookingForm(forms.ModelForm):
             restaurant_id=restaurant.id
         )
 
+        self.restaurant = restaurant
+
     class Meta:
         model = Booking
         fields = ("table", "date", "total_guests")
@@ -55,6 +57,7 @@ class BookingForm(forms.ModelForm):
         date = cleaned_data.get("date")
         total_guests = cleaned_data.get("total_guests")
         table = cleaned_data.get("table")
+        min_guest = self.restaurant.setting.min_guest
 
         if total_guests is not None:
             if total_guests > table.capacity:
@@ -67,8 +70,11 @@ class BookingForm(forms.ModelForm):
                     {"total_guests": ["Cannot book 0 or less guests"]}
                 )
 
+            if total_guests < min_guest:
+                raise ValidationError(
+                    {"total_guests": [f"Minimum guest bookable is: {min_guest}"]}
+                )
+
         if date:
             if date < timezone.now():
-                raise ValidationError(
-                    "Date cannot be in the past", params={"date", date}
-                )
+                raise ValidationError({"date": ["Date cannot be in the past"]})
