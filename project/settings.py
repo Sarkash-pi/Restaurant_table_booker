c3a5523c304a69f94543bbff017770a6b3bbbfbd
Sarkash Talib <sarkash.talib@gmail.com>
2021-12-11 00:18:50 +0000

    Modification commit

--- project/settings.py
+++ project/settings.py
@@ -83,6 +83,17 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "table_booker_db",
+#         "USER": "table_booker_user",
+#         "PASSWORD": "r4$AMGexn9a#4mJd",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
---
+++ secrets.sh
@@ -0,0 +1,1 @@
+export SECRET_KEY='<if-vw=-hhszmeihqqvg!*bxwteda+3dwt&w)07%onk3%h57fh*>'
